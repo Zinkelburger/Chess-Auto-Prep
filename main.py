5f4cf7256114c80a1a6a035a232715ecf830d219
Andrew Bernal <andrewlbernal@gmail.com>
2025-10-26 14:07:28 -0400

    a
    Signed-off-by: Andrew Bernal <andrewlbernal@gmail.com>

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-Chess Auto Prep - Main application entry point.
-
-Clean, idiomatic Python chess application with modern architecture.
-"""
-from windows.main_window import main
-
-if __name__ == "__main__":
-    main()
