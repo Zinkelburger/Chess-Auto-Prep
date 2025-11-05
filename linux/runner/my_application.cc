438ea41cf66286815377e4f04e6c2549837add4f
Andrew Bernal <anbernal@redhat.com>
2025-11-05 12:51:55 -0500

    add knook logo
    Signed-off-by: Andrew Bernal <anbernal@redhat.com>

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -55,6 +55,13 @@ static void my_application_activate(GApplication* application) {
 
   gtk_window_set_default_size(window, 1280, 720);
 
+  // Set window icon
+  g_autoptr(GError) icon_error = nullptr;
+  gtk_window_set_icon_from_file(window, "assets/images/knook.png", &icon_error);
+  if (icon_error != nullptr) {
+    g_warning("Failed to load window icon: %s", icon_error->message);
+  }
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
