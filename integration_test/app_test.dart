b1341b2cef5bf391ac698eb5e8fce5b8efeef548
Zinkelburger <andrewlbernal@gmail.com>
2026-09-13 17:56:59 -0400

    Update desktop creation regression to require staying in trainer

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -159,26 +159,6 @@ void main() {
       expect(find.byType(ChessBoardWidget), findsNothing);
     });
 
-    testWidgets(
-      'trainer creation opens a fresh shared screen and cancels back to trainer',
-      (tester) async {
-        await pumpApp(tester);
-        await tester.tap(find.byKey(AppModeSwitcher.switcherKey));
-        await tester.pumpAndSettle();
-        await tester.tap(
-          find.widgetWithText(MenuItemButton, 'Repertoire trainer'),
-        );
-        await tester.pumpAndSettle();
-        await tester.tap(find.text('Create new repertoire'));
-        await tester.pumpAndSettle();
-        expect(find.byType(RepertoireCreationScreen), findsOneWidget);
-        await tester.tap(find.text('Cancel'));
-        await tester.pumpAndSettle();
-        expect(find.text('Repertoire trainer'), findsWidgets);
-        expect(find.text('Create new repertoire'), findsOneWidget);
-      },
-    );
-
     testWidgets('switches to Repertoire Builder', (tester) async {
       await pumpApp(tester);
 
@@ -224,7 +204,18 @@ void main() {
       expect(find.text('Open PGN file…'), findsOneWidget);
       await tester.tap(find.text('Create new repertoire'));
       await tester.pumpAndSettle();
-      expect(getAppState(tester).currentMode, equals(AppMode.repertoire));
+      expect(
+        getAppState(tester).currentMode,
+        equals(AppMode.repertoireTrainer),
+      );
+      expect(find.byType(RepertoireCreationScreen), findsOneWidget);
+      await tester.tap(find.text('Cancel'));
+      await tester.pumpAndSettle();
+      expect(find.text('Create new repertoire'), findsOneWidget);
+      expect(
+        getAppState(tester).currentMode,
+        equals(AppMode.repertoireTrainer),
+      );
     });
   });
 }
